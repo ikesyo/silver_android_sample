aa245529bcdd31ee1e605c2b4d41c6a4333e9b99
Syo Ikeda <suicaicoca@gmail.com>
2015-09-29 11:02:15 +0900

    Initial commit

---
+++ MainActivity.swift
@@ -0,0 +1,15 @@
+import java.util
+import  android.app
+import  android.content
+import  android.os
+import  android.util
+import  android.view
+import  android.widget
+
+public class MainActivity: Activity {
+
+	public override func onCreate(savedInstanceState: Bundle!) {
+		super.onCreate(savedInstanceState)
+		ContentView = R.layout.main
+	}
+}
