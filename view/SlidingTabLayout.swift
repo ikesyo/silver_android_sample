473f5f032e118c341e7ee27cb0f2af34fed26a6a
Syo Ikeda <suicaicoca@gmail.com>
2015-09-29 13:06:49 +0900

    Add common/view

---
+++ view/SlidingTabLayout.swift
@@ -0,0 +1,235 @@
+﻿//  /*
+//  * Copyright (C) 2013 The Android Open Source Project
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *	  http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+import android.content
+import android.graphics
+import android.os
+import android.support.v4.view
+import android.util
+import android.view
+import android.widget
+
+//  namespace com.example.android.common.view
+//  /**
+//  * To be used with ViewPager to provide a tab indicator component which give constant feedback as to
+//  * the user's scroll progress.
+//  * <p>
+//  * To use the component, simply add it to your view hierarchy. Then in your
+//  * {@link android.app.Activity} or {@link android.support.v4.app.Fragment} call
+//  * {@link #setViewPager(ViewPager)} providing it the ViewPager this layout is being used for.
+//  * <p>
+//  * The colors can be customized in two ways. The first and simplest is to provide an array of colors
+//  * via {@link #setSelectedIndicatorColors(int...)} and {@link #setDividerColors(int...)}. The
+//  * alternative is via the {@link TabColorizer} interface which provides you complete control over
+//  * which color is used for any individual position.
+//  * <p>
+//  * The views used as tabs can be customized by calling {@link #setCustomTabView(int, int)},
+//  * providing the layout ID of your custom layout.
+//  */
+class SlidingTabLayout: HorizontalScrollView {
+	//  /**
+	//  * Allows complete control over the colors drawn in the tab layout. Set with
+	//  * {@link #setCustomTabColorizer(TabColorizer)}.
+	//  */
+	protocol TabColorizer {
+		func getIndicatorColor(_ position: Int32) -> Int32
+		func getDividerColor(_ position: Int32) -> Int32
+	}
+
+	static let TITLE_OFFSET_DIPS: Int32 = 24
+	static let TAB_VIEW_PADDING_DIPS: Int32 = 16
+	static let TAB_VIEW_TEXT_SIZE_SP: Int32 = 12
+	var mTitleOffset: Int32
+	var mTabViewLayoutId: Int32 = 0
+	var mTabViewTextViewId: Int32 = 0
+	var mViewPager: ViewPager!
+	var mViewPagerPageChangeListener: ViewPager.OnPageChangeListener!
+	var mTabStrip: SlidingTabStrip!
+
+	convenience init(_ context: Context!) {
+		self.init(context, nil)
+	}
+
+	convenience init(_ context: Context!, _ attrs: AttributeSet!) {
+		self.init(context, attrs, 0)
+	}
+
+	init(_ context: Context!, _ attrs: AttributeSet!, _ defStyle: Int32) {
+		super.init(context, attrs, defStyle)
+		//  // Disable the Scroll Bar
+		setHorizontalScrollBarEnabled(false)
+		//  // Make sure that the Tab Strips fills this View
+		setFillViewport(true)
+		mTitleOffset = (TITLE_OFFSET_DIPS * Int32(getResources().getDisplayMetrics().density))
+		mTabStrip = SlidingTabStrip(context)
+		addView(mTabStrip, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+	}
+
+	func setCustomTabColorizer(_ tabColorizer: TabColorizer!) {
+		mTabStrip.setCustomTabColorizer(tabColorizer)
+	}
+
+	func setSelectedIndicatorColors(_ colors: Int32[]!) {
+		mTabStrip.setSelectedIndicatorColors(colors)
+	}
+
+	func setDividerColors(_ colors: Int32[]!) {
+		mTabStrip.setDividerColors(colors)
+	}
+
+	func setOnPageChangeListener(_ listener: ViewPager.OnPageChangeListener!) {
+		mViewPagerPageChangeListener = listener
+	}
+
+	func setCustomTabView(_ layoutResId: Int32, _ textViewId: Int32) {
+		mTabViewLayoutId = layoutResId
+		mTabViewTextViewId = textViewId
+	}
+
+	func setViewPager(_ viewPager: ViewPager!) {
+		mTabStrip.removeAllViews()
+		mViewPager = viewPager
+		if viewPager != nil {
+			viewPager.setOnPageChangeListener(InternalViewPagerListener(self))
+			populateTabStrip()
+		}
+	}
+
+	func createDefaultTabView(_ context: Context!) -> TextView! {
+		var textView: TextView! = TextView(context)
+		textView.setGravity(Gravity.CENTER)
+		textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, TAB_VIEW_TEXT_SIZE_SP)
+		textView.setTypeface(Typeface.DEFAULT_BOLD)
+		if Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB {
+			//  // If we're running on Honeycomb or newer, then we can use the Theme's
+			//  // selectableItemBackground to ensure that the View has a pressed state
+			var outValue: TypedValue! = TypedValue()
+			getContext().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
+			textView.setBackgroundResource(outValue.resourceId)
+		}
+		if Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH {
+			//  // If we're running on ICS or newer, enable all-caps to match the Action Bar tab style
+			textView.setAllCaps(true)
+		}
+		var padding: Int32 = (TAB_VIEW_PADDING_DIPS * Int32(getResources().getDisplayMetrics().density)) 
+		textView.setPadding(padding, padding, padding, padding)
+		return textView
+	}
+
+	func populateTabStrip() {
+		var adapter: PagerAdapter! = mViewPager.getAdapter()
+		var tabClickListener: View.OnClickListener! = TabClickListener(self)
+		for i in 0..<adapter.getCount() {
+			var tabView: View! = nil
+			var tabTitleView: TextView! = nil
+			if mTabViewLayoutId != 0 {
+				//  // If there is a custom tab view layout id set, try and inflate it
+				tabView = LayoutInflater.from(getContext()).inflate(mTabViewLayoutId, mTabStrip, false)
+				tabTitleView = tabView.findViewById(mTabViewTextViewId) as? TextView
+			}
+			if tabView == nil {
+				tabView = createDefaultTabView(getContext())
+			}
+			if (tabTitleView == nil) && tabView is TextView {
+				tabTitleView = tabView as? TextView
+			}
+			tabTitleView.setText(adapter.getPageTitle(i))
+			tabView.setOnClickListener(tabClickListener)
+			mTabStrip.addView(tabView)
+		}
+	}
+
+	override func onAttachedToWindow() {
+		super.onAttachedToWindow()
+		if mViewPager != nil {
+			scrollToTab(mViewPager.getCurrentItem(), 0)
+		}
+	}
+
+	func scrollToTab(_ tabIndex: Int32, _ positionOffset: Int32) {
+		var tabStripChildCount: Int32 = mTabStrip.getChildCount()
+		if ((tabStripChildCount == 0) | (tabIndex < 0)) | (tabIndex >= tabStripChildCount) {
+			return
+		}
+		var selectedChild: View! = mTabStrip.getChildAt(tabIndex)
+		if selectedChild != nil {
+			var targetScrollX: Int32 = selectedChild.getLeft() + positionOffset
+			if (tabIndex > 0) | (positionOffset > 0) {
+				//  // If we're not at the first child and are mid-scroll, make sure we obey the offset
+				targetScrollX -= mTitleOffset
+			}
+			scrollTo(targetScrollX, 0)
+		}
+	}
+
+	class InternalViewPagerListener: ViewPager.OnPageChangeListener {
+		var mScrollState: Int32 = 0
+		var mParent: SlidingTabLayout!
+
+		init(_ parent: SlidingTabLayout!) {
+			mParent = parent
+		}
+
+		func onPageScrolled(_ position: Int32, _ positionOffset: Float, _ positionOffsetPixels: Int32) {
+			var tabStripChildCount: Int32 = mParent.mTabStrip.getChildCount()
+			if ((tabStripChildCount == 0) | (position < 0)) | (position >= tabStripChildCount) {
+				return
+			}
+			mParent.mTabStrip.onViewPagerPageChanged(position, positionOffset)
+			var selectedTitle: View! = mParent.mTabStrip.getChildAt(position)
+			var extraOffset: Int32 = 0
+			if selectedTitle != nil { extraOffset = Int(positionOffset) * selectedTitle.getWidth() }
+			mParent.scrollToTab(position, extraOffset)
+			if mParent.mViewPagerPageChangeListener != nil {
+				mParent.mViewPagerPageChangeListener.onPageScrolled(position, positionOffset, positionOffsetPixels)
+			}
+		}
+
+		func onPageScrollStateChanged(_ state: Int32) {
+			mScrollState = state
+			if mParent.mViewPagerPageChangeListener != nil {
+				mParent.mViewPagerPageChangeListener.onPageScrollStateChanged(state)
+			}
+		}
+
+		func onPageSelected(_ position: Int32) {
+			if mScrollState == ViewPager.SCROLL_STATE_IDLE {
+				mParent.mTabStrip.onViewPagerPageChanged(position, 0.0)
+				mParent.scrollToTab(position, 0)
+			}
+			if mParent.mViewPagerPageChangeListener != nil {
+				mParent.mViewPagerPageChangeListener.onPageSelected(position)
+			}
+		}
+	}
+
+	class TabClickListener: View.OnClickListener {
+		var mParent: SlidingTabLayout!
+
+		init(_ parent: SlidingTabLayout!) {
+			mParent = parent
+		}
+
+		func onClick(_ v: View!) {
+			for i in 0..<mParent.mTabStrip.getChildCount() {
+				if v == mParent.mTabStrip.getChildAt(i) {
+					mParent.mViewPager.setCurrentItem(i)
+					return
+				}
+			}
+		}
+	}
+
+}
---
+++ view/SlidingTabStrip.swift
@@ -0,0 +1,162 @@
+﻿//  /*
+//  * Copyright (C) 2013 The Android Open Source Project
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *	  http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+import android
+import android.content
+import android.graphics
+import android.util
+import android.view
+import android.widget
+
+//  namespace com.example.android.common.view
+class SlidingTabStrip: LinearLayout {
+	static let DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS: Int32 = 2
+	static let DEFAULT_BOTTOM_BORDER_COLOR_ALPHA: Int8 = 38
+	static let SELECTED_INDICATOR_THICKNESS_DIPS: Int32 = 8
+	static let DEFAULT_SELECTED_INDICATOR_COLOR: Int64 = 4281578981
+	static let DEFAULT_DIVIDER_THICKNESS_DIPS: Int32 = 1
+	static let DEFAULT_DIVIDER_COLOR_ALPHA: Int8 = 32
+	static let DEFAULT_DIVIDER_HEIGHT: Float = 0.5
+	var mBottomBorderThickness: Int32
+	var mBottomBorderPaint: Paint!
+	var mSelectedIndicatorThickness: Int32
+	var mSelectedIndicatorPaint: Paint!
+	var mDefaultBottomBorderColor: Int32
+	var mDividerPaint: Paint!
+	var mDividerHeight: Float
+	var mSelectedPosition: Int32 = 0
+	var mSelectionOffset: Float = 0
+	var mCustomTabColorizer: SlidingTabLayout.TabColorizer!
+	var mDefaultTabColorizer: SimpleTabColorizer!
+
+	convenience init(_ context: Context!) {
+		self.init(context, nil)
+	}
+
+	init(_ context: Context!, _ attrs: AttributeSet!) {
+		super.init(context, attrs)
+		setWillNotDraw(false)
+		var density: Int32 = Int32(getResources().getDisplayMetrics().density)
+		var outValue: TypedValue! = TypedValue()
+		context.getTheme().resolveAttribute(android.R.attr.colorForeground, outValue, true)
+		var themeForegroundColor: Int32 = outValue.data
+		mDefaultBottomBorderColor = setColorAlpha(themeForegroundColor, DEFAULT_BOTTOM_BORDER_COLOR_ALPHA)
+		mDefaultTabColorizer = SimpleTabColorizer()
+		mDefaultTabColorizer.setIndicatorColors([DEFAULT_SELECTED_INDICATOR_COLOR])
+		mDefaultTabColorizer.setDividerColors([setColorAlpha(themeForegroundColor, DEFAULT_DIVIDER_COLOR_ALPHA)])
+		mBottomBorderThickness = DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS * density 
+		mBottomBorderPaint = Paint()
+		mBottomBorderPaint.setColor(mDefaultBottomBorderColor)
+		mSelectedIndicatorThickness = SELECTED_INDICATOR_THICKNESS_DIPS * density 
+		mSelectedIndicatorPaint = Paint()
+		mDividerHeight = DEFAULT_DIVIDER_HEIGHT
+		mDividerPaint = Paint()
+		mDividerPaint.setStrokeWidth(DEFAULT_DIVIDER_THICKNESS_DIPS * density)
+	}
+
+	func setCustomTabColorizer(_ customTabColorizer: SlidingTabLayout.TabColorizer!) {
+		mCustomTabColorizer = customTabColorizer
+		invalidate()
+	}
+
+	func setSelectedIndicatorColors(_ colors: Int32[]!) {
+		//  // Make sure that the custom colorizer is removed
+		mCustomTabColorizer = nil
+		mDefaultTabColorizer.setIndicatorColors(colors)
+		invalidate()
+	}
+
+	func setDividerColors(_ colors: Int32[]!) {
+		//  // Make sure that the custom colorizer is removed
+		mCustomTabColorizer = nil
+		mDefaultTabColorizer.setDividerColors(colors)
+		invalidate()
+	}
+
+	func onViewPagerPageChanged(_ position: Int32, _ positionOffset: Float) {
+		mSelectedPosition = position
+		mSelectionOffset = positionOffset
+		invalidate()
+	}
+
+	override func onDraw(_ canvas: Canvas!) {
+		var height: Int32 = getHeight()
+		var childCount: Int32 = getChildCount()
+		var dividerHeightPx: Int32 = Int32(Math.min(Math.max(0.0, mDividerHeight), 1.0)) * height 
+		var tabColorizer: SlidingTabLayout.TabColorizer! = mCustomTabColorizer != nil ? mCustomTabColorizer : mDefaultTabColorizer
+		//  // Thick colored underline below the current selection
+		if childCount > 0 {
+			var selectedTitle: View! = getChildAt(mSelectedPosition)
+			var `left`: Int32 = selectedTitle.getLeft()
+			var `right`: Int32 = selectedTitle.getRight()
+			var color: Int32 = tabColorizer.getIndicatorColor(mSelectedPosition)
+			if (mSelectionOffset > 0.0) & (mSelectedPosition < (getChildCount() - 1)) {
+				var nextColor: Int32 = tabColorizer.getIndicatorColor(mSelectedPosition + 1)
+				if color != nextColor {
+					color = blendColors(nextColor, color, mSelectionOffset)
+				}
+				//  // Draw the selection partway between the tabs
+				var nextTitle: View! = getChildAt(mSelectedPosition + 1)
+				left = ((Int(mSelectionOffset) * nextTitle.getLeft()) + ((1 - Int(mSelectionOffset)) * `left`)) 
+				right = ((Int(mSelectionOffset) * nextTitle.getRight()) + ((1 - Int(mSelectionOffset)) * `right`)) 
+			}
+			mSelectedIndicatorPaint.setColor(color)
+			canvas.drawRect(`left`, height - mSelectedIndicatorThickness, `right`, height, mSelectedIndicatorPaint)
+		}
+		//  // Thin underline along the entire bottom edge
+		canvas.drawRect(0, height - mBottomBorderThickness, getWidth(), height, mBottomBorderPaint)
+		//  // Vertical separators between the titles
+		var separatorTop: Int32 = (height - dividerHeightPx) / 2
+		for i in 0..<childCount {
+			var child: View! = getChildAt(i)
+			mDividerPaint.setColor(tabColorizer.getDividerColor(i))
+			canvas.drawLine(child.getRight(), separatorTop, child.getRight(), separatorTop + dividerHeightPx, mDividerPaint)
+		}
+	}
+
+	class func setColorAlpha(_ color: Int32, _ alpha: Int8) -> Int32 {
+		return Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color))
+	}
+
+	class func blendColors(_ color1: Int32, _ color2: Int32, _ ratio: Float) -> Int32 {
+		var inverseRation: Float = 1.0 - ratio
+		var r: Float = (Color.red(color1) * ratio) + (Color.red(color2) * inverseRation)
+		var g: Float = (Color.green(color1) * ratio) + (Color.green(color2) * inverseRation)
+		var b: Float = (Color.blue(color1) * ratio) + (Color.blue(color2) * inverseRation)
+		return Color.rgb(Int(r), Int(g), Int(b))
+	}
+
+	class SimpleTabColorizer: SlidingTabLayout.TabColorizer {
+		var mIndicatorColors: Int32[]!
+		var mDividerColors: Int32[]!
+
+		func getIndicatorColor(_ position: Int32) -> Int32 {
+			return mIndicatorColors[position % mIndicatorColors.length]
+		}
+
+		func getDividerColor(_ position: Int32) -> Int32 {
+			return mDividerColors[position % mDividerColors.length]
+		}
+
+		func setIndicatorColors(_ colors: Int32[]!) {
+			mIndicatorColors = colors
+		}
+
+		func setDividerColors(_ colors: Int32[]!) {
+			mDividerColors = colors
+		}
+	}
+
+}
